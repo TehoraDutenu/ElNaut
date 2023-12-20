fd622954df87659ce1b96209443aafb04f39cf7d
TehoraDutenu <tehoradutenu@gmail.com>
2023-12-20 17:31:21 +0100

    Mobile 1rst ok

--- wp-content/themes/nautilus/index.php
+++ wp-content/themes/nautilus/index.php
@@ -13,10 +13,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['a
 <main>
     <div class="main-container">
 
-        <!-- <div class="img-container">
-        <img src="<?php echo get_template_directory_uri() . '/images/imageSpace.jpg' ?>" class="index-img img-fluid" alt="Espaces du Nautilus">
-        <a href="#" type="button" class="votre-bouton">Plongez dans l'univers du Nautilus</a>
-    </div> -->
+        <div class="img-container">
+            <img src="<?php echo get_template_directory_uri() . '/images/imageSpace.jpg' ?>" class="index-img img-fluid" alt="Espaces du Nautilus">
+        </div>
 
         <div class="agenda-container">
             <iframe style="width:100%;" frameborder="0" scrolling="yes" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/21429945/embeds/4880713/events?lang=fr" data-lang="fr"></iframe>
