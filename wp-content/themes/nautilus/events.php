5a003e75daf4704e5f6cc45d7f4ea0f633a4d266
TehoraDutenu <tehoradutenu@gmail.com>
2023-12-27 14:29:16 +0100

    test2

---
+++ wp-content/themes/nautilus/events.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Template Name: Rents
+ */
+?>
+
+<?php get_header() ?>
+
+<main style="background-color: #f3dcb0;">
+    <h3>Embarquez à bord</h3>
+    <em>Tous les formats pour tous les besoins</em>
+</main>
+
+<?php get_footer() ?>
+
+<!-- if($envoi) header("location:ma_page_html.html"); -->
