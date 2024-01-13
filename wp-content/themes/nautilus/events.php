b78f6558116ee20e8533f6fe9e9f1fe1ae403f94
TehoraDutenu <tehoradutenu@gmail.com>
2024-01-13 17:56:45 +0100

    events

--- wp-content/themes/nautilus/events.php
+++ wp-content/themes/nautilus/events.php
@@ -13,17 +13,13 @@
                 <iframe style="width:100%;" frameborder="0" scrolling="yes" allowtransparency="allowtransparency" class="cibulFrame cbpgbdy" data-oabdy src="//openagenda.com/agendas/64371698/embeds/19969799/events?lang=fr" data-lang="fr"></iframe>
                 <script type="text/javascript" src="//openagenda.com/js/embed/cibulBodyWidget.js"></script>
             </div>
-            <div class="calendar">
-                <div class="cbpgcl cibulCalendar" data-oacl data-cbctl="64371698/19969799|fr" data-lang="fr"></div>
-                <script type="text/javascript" src="//openagenda.com/js/embed/cibulCalendarWidget.js"></script>
+            <div class="rents-linking">
+                <h4 class="rents-linking-title">Vous avez un projet d'atelier ou d'événement ?</h4>
+                <a href="http://projet.lndo.site/locations-coworking/" class="rents-linking-button" type="button">Rejoignez-nous !</a>
             </div>
             <div class="map">
                 <div class="cbpgmp cibulMap" data-oamp data-cbctl="64371698/19969799" data-lang="fr"></div>
                 <script type="text/javascript" src="//openagenda.com/js/embed/cibulMapWidget.js"></script>
-            </div>
-            <div class="rents-linking">
-                <h4 class="rents-linking-title">Vous avez un projet d'atelier ou d'événement ?</h4>
-                <a href="http://projet.lndo.site/locations-coworking/" class="rents-linking-button" type="button">Rejoignez-nous !</a>
             </div>
         </div>
     </main>
