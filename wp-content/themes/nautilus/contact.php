99e8a67c3437571e31e641146dae9829b8c21e73
TehoraDutenu <tehoradutenu@gmail.com>
2024-01-15 10:04:14 +0100

    contact

--- wp-content/themes/nautilus/contact.php
+++ wp-content/themes/nautilus/contact.php
@@ -5,10 +5,12 @@
 
 get_header(); ?>
 
-<div class="contact-main-container">
-    <h2 class="contact-title">Que peut-on faire pour vous ?</h2>
+<body class="contact-body">
+    <div class="contact-main-container">
+        <h2 class="contact-title">Que peut-on faire pour vous ?</h2>
 
-    <?php echo apply_shortcodes('[contact-form-7 id="b788748" title="Le Nautilus"]'); ?>
-</div>
+        <?php echo apply_shortcodes('[contact-form-7 id="b788748" title="Le Nautilus"]'); ?>
+    </div>
+</body>
 
 <?php get_footer() ?>
